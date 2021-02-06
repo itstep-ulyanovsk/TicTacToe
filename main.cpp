8380d6443202ba07f57cba56fcd3ae6ca266169d
anst-foto <starininandrey@gmail.com>
2021-02-06 08:30:55 +0300

    Добавление игрового поля и проверок ввода координат

--- main.cpp
+++ main.cpp
@@ -4,5 +4,31 @@ using namespace std;
 
 int main()
 {
+    system("chcp 65001");
 
+    const int SIZE = 3;
+    char board[SIZE][SIZE] = {{' ', ' ', ' '},
+                              {' ', ' ', ' '},
+                              {' ', ' ', ' '}};
+
+    int row, col;
+
+    for (int i = 0; i < SIZE; ++i) {
+        for (int j = 0; j < SIZE; ++j) {
+            cout << "|" << board[i][j] << "|";
+        }
+        cout << endl;
+    }
+
+    do {
+        do {
+            cout << "Введите номер строки (от  0 до 2) - ";
+            cin >> row;
+        } while (row < 0 || row > 2);
+
+        do {
+            cout << "Введите номер столбца (от  0 до 2) - ";
+            cin >> col;
+        } while (col < 0 || col > 2);
+    } while (board[row][col] != ' ');
 }
