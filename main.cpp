246be293fcf004f79733a1315476531d258ead6f
Olga_Bulagina <BulaginaOA922@mail.ru>
2021-02-19 13:57:09 +0400

    изменение значений в массиве

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-// test comit
+
 using namespace std;
 
 const int SIZE = 3;
-char board[SIZE][SIZE] = {{' ', ' ', ' '},
-                          {' ', ' ', ' '},
-                          {' ', ' ', ' '}};
+char board[SIZE][SIZE] = { {'1', '2', '3'},
+                          {'4', '5', '6'},
+                          {'7', '8', '9'} };
 
 void PrintBoard()
 {
@@ -103,4 +103,5 @@ int main()
         }
 
     } while (true);
+   
 }
